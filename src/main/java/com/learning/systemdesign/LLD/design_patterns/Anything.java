cac33f997136672374ee6f0b154b9d8e75a17b32
Digvijay Singh <digvijaydj11295@gmail.com>
2025-08-07 18:21:15 +0530

    Initial Commit

---
+++ src/main/java/com/learning/systemdesign/LLD/design_patterns/Anything.java
@@ -0,0 +1,6 @@
+package com.learning.systemdesign.LLD.design_patterns;
+
+public class Anything {
+    // This class is intentionally left empty.
+    // It serves as a placeholder for future design patterns implementations.
+}
